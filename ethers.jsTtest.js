0e152096505bcfcad764884be06ccfb94cf3d61e
xu-bu <36945794+xu-bu@users.noreply.github.com>
2023-08-02 01:44:52 +0100

    rollback

--- ethers.jsTtest.js
+++ ethers.jsTtest.js
@@ -1,6 +1,6 @@
 const { ethers, formatEther } = require("ethers");
 
-const provider = new ethers.JsonRpcProvider(`https://eth-goerli.g.alchemy.com/v2/yioJ74QCa2Dl13upBt8C9gLUc_dFdRn5`)
+const provider = new ethers.JsonRpcProvider(`${YOUR_ALCHEMY_API}`)
 
 const address = '0x52906abb6B9d358eEF7D903cf1ecb521643297f4'
 
